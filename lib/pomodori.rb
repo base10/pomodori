9313d9917fc47c95defd22928a3075ea30e2d7d7
Nathan L. Walls <nathan@wallscorp.us>
2013-05-26 20:48:52 -0400

    Adding an initial test, expecting failure w/o a config

--- lib/pomodori.rb
+++ lib/pomodori.rb
@@ -1,4 +1,5 @@
 require "pomodori/version"
+require "pomodori/config"
 
 module Pomodori
   # Your code goes here...
--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -5,4 +5,8 @@ describe Pomodori::Config do
   it "is setup correctly" do
     expect { 1 }.to be_true
   end
+  
+  it "raises an error without a config file" do
+    expect { Pomodori::Config.new }. to raise_error(Errno::ENOENT)
+  end
 end
