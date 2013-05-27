e5dec495d27468a87facb158cd3f1ee950ee0d5a
Nathan L. Walls <nathan@wallscorp.us>
2013-05-26 21:06:20 -0400

    Removed starter test

--- spec/lib/config_spec.rb
+++ spec/lib/config_spec.rb
@@ -2,10 +2,6 @@ require "spec_helper"
 require "fileutils"
 
 describe Pomodori::Config do
-  it "is setup correctly" do
-    expect { 1 }.to be_true
-  end
-  
   it "raises an error without a config file" do
     expect { Pomodori::Config.new }.to raise_error(Errno::ENOENT)
   end
