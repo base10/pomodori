0e5eea5bf8a3e4a8f3362151fc12b192977c4833
Nathan L. Walls <nathan@wallscorp.us>
2013-05-28 21:24:08 -0400

    Bringing setup and config into require stmts

--- lib/pomodori.rb
+++ lib/pomodori.rb
@@ -1,4 +1,6 @@
 require_relative "pomodori/version"
+require_relative "pomodori/setup"
+require_relative "pomodori/config"
 
 module Pomodori
 end
---
+++ lib/pomodori/setup.rb
@@ -0,0 +1,13 @@
+require_relative "config"
+
+module Pomodori
+  class Setup
+    include Pomodori::Config
+
+    def initialize
+    end
+
+    def run
+    end
+  end
+end
