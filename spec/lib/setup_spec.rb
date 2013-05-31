6bf35eb8eded4f0cc87b3bfa4518c6f424c091c7
Nathan L. Walls <nathan@wallscorp.us>
2013-05-30 21:50:22 -0400

    Refactoring the test config path for safety

--- spec/lib/setup_spec.rb
+++ spec/lib/setup_spec.rb
@@ -2,29 +2,35 @@ require "spec_helper"
 require 'fileutils'
 
 describe Pomodori::Setup do
-  describe "data directory setup" do
-    before(:each) do
-      @setup = Pomodori::Setup.new
-      allow(@setup).to receive(:default_config_path).and_return( 
-                            File.expand_path( "../../dotpomodori", __FILE__ )  
-                          )
-    end
+  let ( :test_config_path ) { File.expand_path( "../../dotpomodori", __FILE__ ) }
 
+  before(:each) do
+    @setup = Pomodori::Setup.new
+    allow(@setup).to receive(:default_config_path).and_return( test_config_path )
+  end
+
+  after(:each) do
+    FileUtils.rm_rf test_config_path
+  end
+
+  describe "data directory setup" do
     it "creates the .pomodori folder if it doesn't exist" do
       expect { @setup.ensure_config_path_exists }.to_not raise_error
       expect( File.directory?( @setup.default_config_path ) ).to be(true)
     end
-
-    after(:each) do
-      FileUtils.rm_rf @setup.default_config_path
-    end
   end
 
-  describe "write config file" do
-  end
+#   describe "write config file" do
+#     it "creates the config file if it doesn't exist" do
+#       pending
+#     end
+# 
+#     it "respects an existing config file" do
+#       pending
+#     end
+#   end
 
   # Find database state
   # Create the database
   # Run database migrations
-
 end
