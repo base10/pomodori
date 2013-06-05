143f97dfbf04a49b56ac1fd6439bf50a30cffc1e
Nathan L. Walls <nathan@wallscorp.us>
2013-06-04 22:36:49 -0400

    Taping out the Event base class

--- lib/pomodori/event.rb
+++ lib/pomodori/event.rb
@@ -2,6 +2,22 @@
 # TODO: Acceptable states will be defined. 
 # In both cases above, this is to work around sqlite's lack of enum fields
 
-
 # kinds:    'pomodoro', 'break', 'long_break'
 # States:   'complete', 'aborted', 'in_progress'
+
+module Pomodori
+  class Event
+    include Pomodori::Config
+
+    attr_accessor :notifications, :summary, :duration, 
+                  :started_at, :completed_at
+
+    def kind
+      
+    end
+
+    def state
+      
+    end
+  end
+end
