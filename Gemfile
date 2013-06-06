c13b8bb8308de8b6fb37cd1414e4c108167a324d
Nathan L. Walls <nathan@wallscorp.us>
2013-06-05 21:55:58 -0400

    Time to start writing business objects, so time for factories

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gemspec
 # guard, spork and others are "nice to have"
 
 gem "rspec",        '~> 2.14rc1'
+gem "factory_girl"
 gem "guard-bundler"
 gem "guard-rspec"
 gem "guard-spork"
---
+++ spec/factories.rb
@@ -0,0 +1,28 @@
+FactoryGirl.define do
+  factory :pomodoro do
+    summary       'Starting Project Lorum Ipsum'
+    duration      '25'
+    kind          'pomodoro'
+  
+    started_at    '2013-06-05T22:07:19-04:00'
+    completed_at  '2013-06-05T22:32:19-04:00'
+  end
+
+  factory :break do
+    summary       'Break time!'
+    duration      '5'
+    kind          'break'
+  
+    started_at    '2013-06-05T22:32:20-04:00'
+    completed_at  '2013-06-05T22:37:20-04:00'
+  end
+
+  factory :long_break do
+    summary       'Take a walk!'
+    duration      '15'
+    kind          'long_break'
+  
+    started_at    '2013-06-06T19:06:13-04:00'
+    completed_at  '2013-06-06T19:21:13-04:00'
+  end
+end
