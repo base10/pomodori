01ca4d35f32615e1a025e4dc2ab9fc9bd3f39d4e
Nathan L. Walls <nathan@wallscorp.us>
2013-08-31 21:12:36 -0400

    Added TODO for class method to return correct break type

--- lib/pomodori/pausa.rb
+++ lib/pomodori/pausa.rb
@@ -1,4 +1,6 @@
 module Pomodori
   class Pausa < Pomodori::Event
+    # TODO: class method that determines the next type of break and returns
+    # the appropriate object (pausa or lunga_pausa)
   end
 end
