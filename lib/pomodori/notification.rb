a57bbf0273c1f25727e527a227fee1f9227b72b4
Nathan L. Walls <nathan@wallscorp.us>
2013-12-22 17:07:06 -0500

    Documented Pomodori::Notification
    - #16

--- lib/pomodori/notification.rb
+++ lib/pomodori/notification.rb
@@ -9,17 +9,62 @@ module Pomodori
   database      = Pomodori::Database.new( { configuration: CONFIGURATION } )
   DB            = database.connect
 
+  # Builds a new Pomodori::Notification object to handles notifications
+  # from events
   class Notification < Sequel::Model
     many_to_one :event
 
     attr_accessor :output
 
+    # Builds a new Pomodori::Notification object. These are tied to Events.
+    # @param options [Hash]
+    #   - action: [String] What action the notification is for (starting, finishing, canceling, etc)
+    #   - deliver_at: [DateTime] When the notification should be delivered
+    #   - event: [Pomodori::Pomodoro, Pomodori::Pausa, Pomodori::LungoPausa]
+    #   - output: [IO, nil] Defaults to STDOUT. Otherwise useful for unit testing
+    # @return A new Pomodori::Notification object
     def initialize( options = {} )
       @output = options.fetch(:output, STDOUT)
 
       super
     end
 
+    # Determines which notifier to use to deliver the notification
+    # @return [String] classname of the Notifier to use
+    def notifier_strategy
+      strategy_pref   = CONFIGURATION.config['notifier']
+      strategy_class  = 'Pomodori::Notifier::' + strategy_pref.camelcase
+    end
+
+    # Delivers a notification with the appropriate notifier and marks
+    # the notification as complete
+    def deliver
+      options = {
+                  notification: self,
+                  output:       output
+                }
+
+      notifier = eval(notifier_strategy).new( options )
+      notifier.deliver
+
+      self.completed_at = DateTime.now
+      self.save
+    end
+
+    # Provides a title for the notification
+    def title
+      "#{event.kind} #{action}"
+    end
+
+    # The core message of the notification
+    def message
+      "#{event.kind}"
+    end
+
+    # Public API ends here
+    protected
+
+    # Ensure we have a valid object
     def validate
       validate_action
       validate_deliver_at
@@ -43,32 +88,6 @@ module Pomodori
       end
 
       errors.add(:event_id, "can't be nil") if event_id.nil?
-    end
-
-    def notifier_strategy
-      strategy_pref   = CONFIGURATION.config['notifier']
-      strategy_class  = 'Pomodori::Notifier::' + strategy_pref.camelcase
-    end
-
-    def deliver
-      options = {
-                  notification: self,
-                  output:       output
-                }
-
-      notifier = eval(notifier_strategy).new( options )
-      notifier.deliver
-
-      self.completed_at = DateTime.now
-      self.save
-    end
-
-    def title
-      "#{event.kind} #{action}"
-    end
-
-    def message
-      "#{event.kind}"
     end
   end
 end
