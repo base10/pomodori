756d3eb00d2a04e0455699bca4dad89e9147b559
Nathan L. Walls <nathan@wallscorp.us>
2013-12-22 16:18:58 -0500

    Documented Pomodori::Notifier
    - #16

--- lib/pomodori/notifier.rb
+++ lib/pomodori/notifier.rb
@@ -1,12 +1,19 @@
 module Pomodori
+  # Base module for delivering notifications
   module Notifier
     attr_accessor :notification, :output
 
+    # Builds a Pomodori::Notifier object (this is a base class)
+    # @param options [Hash]
+    #   - notification: A Pomodori::Notification object
+    #   - output: Where a notification is going to go (defaults to STDOUT)
+    #       Used primarily for testing
     def initialize( options )
       @notification = options.fetch(:notification)
       @output       = options.fetch(:output) { STDOUT }
     end
 
+    # API declaration. See concrete classes.
     def deliver
       raise Pomodori::Notifier::Error, "This method needs to be overwritten"
     end
