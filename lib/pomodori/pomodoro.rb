44008e9420a0fdc61be90399206ea476a80b6859
Nathan L. Walls <nathan@wallscorp.us>
2014-03-12 21:29:09 -0400

    Adding start notifications for pomodoro
    - #26
    - Only start notifications, only for pomodoro
    - Assuming these will be 25 minutes, not what's in the config
    - Seems like this should move to a factory

--- lib/pomodori/pomodoro.rb
+++ lib/pomodori/pomodoro.rb
@@ -5,6 +5,30 @@ module Pomodori
   # See Pomodori::Event for interface info
   class Pomodoro < Pomodori::Event
     def add_start_notifications
+      now = DateTime.now
+      n1  = Notification.new({  action:     "Started",
+                                deliver_at: now,
+                                event:      self
+                            })
+
+      n2  = Notification.new({  action:     "Halfway!",
+                                deliver_at: now + 13.minutes,
+                                event:      self
+                            })
+
+      n3  = Notification.new({  action:     "Almost Done",
+                                deliver_at: now + 20.minutes,
+                                event:      self
+                            })
+
+      n4  = Notification.new({  action:     "Done",
+                                deliver_at: now + 25.minutes,
+                                event:      self
+                             })
+
+      [n1, n2, n3, n4].each do |notice|
+        notice.save
+      end
     end
 
     def add_cancel_notifications
