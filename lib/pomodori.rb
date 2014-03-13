03b20ebc599d91fe0773505409caa09017613493
Nathan L. Walls <nathan@wallscorp.us>
2014-03-12 21:26:11 -0400

    Using more notifications, I need to make sure they're available
    - #26

--- lib/pomodori.rb
+++ lib/pomodori.rb
@@ -5,6 +5,11 @@ require_relative "pomodori/event"
 require_relative "pomodori/pomodoro"
 require_relative "pomodori/pausa"
 require_relative "pomodori/lunga_pausa"
+require_relative "pomodori/notification"
+require_relative "pomodori/notifier"
+require_relative "pomodori/notifier/error"
+require_relative "pomodori/notifier/osx" ## FIXME: Only on Mac OS X
+require_relative "pomodori/notifier/stdout"
 require_relative "string_underscore"
 
 # Everything is a level down.
