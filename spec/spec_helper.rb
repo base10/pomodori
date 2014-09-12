a271dbcf15e387b4c23e9a9060c8c2608b52ddae
Nathan L. Walls <nathan@wallscorp.us>
2014-09-11 21:48:11 -0400

    Addressing RSpec 2.99/3.0 deprecation
    - #28

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,6 @@ Spork.prefork do
       config.syntax = [:expect, :should]
     end
 
-    config.treat_symbols_as_metadata_keys_with_true_values = true
     config.run_all_when_everything_filtered = true
     config.filter_run_including :focus => true
 
