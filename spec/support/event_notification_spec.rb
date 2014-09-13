424831c2670bb4ed8a847bfb3d5299b489833cff
Nathan L. Walls <nathan@wallscorp.us>
2014-09-12 21:55:40 -0400

    Found support way to write "processes state_notifications" example
    - #30
    - Fixes deprecation between RSpec 2.14 and 2.99

--- spec/support/event_notification_spec.rb
+++ spec/support/event_notification_spec.rb
@@ -7,12 +7,11 @@ shared_examples "event state notifications" do
 
   # Solving based on http://stackoverflow.com/questions/9800992/how-to-say-any-instance-should-receive-any-number-of-times-in-rspec/9998793#9998793
   it "processes state_notifications" do
-    count = 0
+    context_object.state_notifications.each do |notification|
+      expect( notification ).to receive( :process ).exactly( :once )
+    end
 
-    Pomodori::Notification.any_instance.stub(:process) { |arg| count += 1 }
     context_object.send( state_action )
-
-    expect(count).to be >= num_notices
   end
 
   it "clears state_notifications" do
